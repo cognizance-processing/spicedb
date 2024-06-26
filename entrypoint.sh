e8bc2d97a30ceaa0429ca16e96dd281b3d78c8c1
slinkiecici <slinkiecici@gmail.com>
2024-06-26 14:55:05 +0200

    adding more params to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,12 @@
 
 # Access the environment variable
 PRESHAREDKEY=${PRESHAREDKEY}
+DBUSER=${DBUSER}
+DBPASSWORD=${DBPASSWORD}
+DBIP=${DBIP}
 
 # Replace the placeholder in your command with the environment variable
 exec /app/spicedb serve \
     --grpc-preshared-key "${PRESHAREDKEY}" \
     --datastore-engine=postgres \
-    --datastore-conn-uri="postgres://new:Happy456@34.44.110.10:5432/spicedb?sslmode=disable"
+    --datastore-conn-uri="postgres://${DBUSER}:${DBPASSWORD}@${DBIP}:5432/spicedb?sslmode=disable"
