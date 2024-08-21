8ae10101507cb17dbee91b3987de62f42563a081
Jordan Kirkbride <jordankirkbride7@gmail.com>
2024-08-21 14:09:19 +0200

    Updating Entrypoint Command

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ DBPASSWORD=${DBPASSWORD}
 DBIP=${DBIP}
 
 # Replace the placeholder in your command with the environment variable
-exec /app/spicedb serve \
+exec /app/spicedb migrate head \
     --grpc-preshared-key "${PRESHAREDKEY}" \
     --datastore-engine=postgres \
     --datastore-conn-uri="postgres://${DBUSER}:${DBPASSWORD}@${DBIP}:5432/spicedb?sslmode=disable"
