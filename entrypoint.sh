3c2c893be56e96c0da35cf3ade062a4c0835d1eb
Jordan Kirkbride <jordankirkbride7@gmail.com>
2024-08-21 14:13:04 +0200

    Removed PRESHAREDKEY environment

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,5 @@ DBIP=${DBIP}
 
 # Replace the placeholder in your command with the environment variable
 exec /app/spicedb migrate head \
-    --grpc-preshared-key "${PRESHAREDKEY}" \
     --datastore-engine=postgres \
     --datastore-conn-uri="postgres://${DBUSER}:${DBPASSWORD}@${DBIP}:5432/spicedb?sslmode=disable"
